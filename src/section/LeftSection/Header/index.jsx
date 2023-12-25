870f8e5efd05e03e2e97f881be98c4ba2012b3da
Sitouthay98 <Sitouthay98@users.noreply.github.com>
2023-12-25 15:49:25 +0700

    changed header desc

--- src/section/LeftSection/Header/index.jsx
+++ src/section/LeftSection/Header/index.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <div className='flex flex-col gap-2'>
                 <div className='text-3xl text-primaryTitle font-bold dark:text-darkTitle'>{data.name}</div>
                 <div className='text-xl font-semibold'>{data.position}</div>
-                <div className='text-sm w-5/6'>{data.caption[0]}<br/>{data.caption[1]}</div>
+                <div className='text-sm w-5/6'>{data.caption}</div>
                 <div className='mt-2'>
                 </div>
             </div>
