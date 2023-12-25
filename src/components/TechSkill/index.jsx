360b5fd2322bbfb7591eabf5a2b8232b7783da3b
Sitouthay98 <Sitouthay98@users.noreply.github.com>
2023-12-25 15:44:47 +0700

    fix white space

--- src/components/TechSkill/index.jsx
+++ src/components/TechSkill/index.jsx
@@ -1,7 +1,7 @@
 const TechSkill = ({ isHighLight, data }) =>{
     return (
         <div>
-            <div className='flex gap-2 text-sm pt-3 ml-5'>
+            <div className='flex flex-wrap gap-2 text-sm pt-3 ml-5'>
             {
                 data.map((e, i) => (
                     <div key={`${e}-tech-${i}`} className={`text-darkTitle bg-primaryText dark:bg-slate-500 dark:text-primaryTitle px-2 py-1 rounded-md ${isHighLight ? "font-semibold" : ""}`}>{e}</div>
